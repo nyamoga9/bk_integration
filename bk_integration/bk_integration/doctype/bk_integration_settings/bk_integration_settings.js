b0af60aaada6305b9b407d68c3dad482989022cf
Michel Baradosa <nyamoga9@gmail.com>
2025-11-27 17:48:56 -0500

    Settings doctype
    Created Settings Doctype

---
+++ bk_integration/bk_integration/doctype/bk_integration_settings/bk_integration_settings.js
@@ -0,0 +1,2 @@
+// BK Integration Settings client script (optional)
+// You can leave this blank for now.
---
+++ bk_integration/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "BK Integration",
+            "color": "blue",
+            "icon": "octicon octicon-link",
+            "type": "module",
+            "label": _("BK Integration")
+        }
+    ]
