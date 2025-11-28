472bf2019dca438150e5c7a78ec29ed7955ce809
Michel Baradosa <nyamoga9@gmail.com>
2025-11-27 19:11:21 -0500

    Update desktop.py

--- bk_integration/config/desktop.py
+++ bk_integration/config/desktop.py
@@ -3,10 +3,15 @@ from frappe import _
 def get_data():
     return [
         {
-            "module_name": "BK Integration",
-            "color": "blue",
-            "icon": "octicon octicon-link",
-            "type": "module",
             "label": _("BK Integration"),
+            "icon": "octicon octicon-credit-card",
+            "items": [
+                {
+                    "type": "doctype",
+                    "name": "BK Integration Settings",
+                    "label": _("BK Integration Settings"),
+                    "description": _("Configure BK API integration."),
+                }
+            ],
         }
     ]
